a044c9de4bce8d3d0435f65495da340c5528eab2
Alsaad Ahamed <alsaadahamed4@gmail.com>
2020-07-14 16:10:20 +0530

    Class based state managemnt.
    - using setState()

--- src/Counter.js
+++ src/Counter.js
@@ -1,14 +1,51 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
+
+const getStateFromLocal = () => {
+  const storage = localStorage.getItem('counterState');
+  if (storage) return JSON.parse(storage);
+  return { count: 0 };
+}
+
+const setStateFromLocal = (state) => {
+  localStorage.setItem('counterState', JSON.stringify(state));
+  document.title = `count(${state.count})`;
+}
 
 class Counter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = getStateFromLocal();
+    this.onIncrement = this.onIncrement.bind(this);
+    this.onDecrement = this.onDecrement.bind(this);
+    this.onReset = this.onReset.bind(this);
+  }
+
+  updateDocumentTile() {
+    document.title = `count(${this.state.count})`
+  }
+
+  onIncrement() {
+    this.setState((state, props) => {
+      if (state.count >= props.max) return;
+      else {
+        return { count: state.count + props.step };
+      }
+    }, () => setStateFromLocal(this.state));
+  }
+  onDecrement() {
+    this.setState({ count: this.state.count - 1 }, this.updateDocumentTile);
+  }
+  onReset() {
+    this.setState({ count: 0 }, this.updateDocumentTile);
+  }
   render() {
     return (
       <div className="Counter">
-        <p className="count">0</p>
+        <p className="count">{this.state.count}</p>
         <section className="controls">
-          <button>Increment</button>
-          <button>Decrement</button>
-          <button>Reset</button>
+          <button onClick={this.onIncrement}>Increment</button>
+          <button onClick={this.onDecrement}>Decrement</button>
+          <button onClick={this.onReset}>Reset</button>
         </section>
       </div>
     );
