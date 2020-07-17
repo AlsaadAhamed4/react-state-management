df3d2cfb89f47ddb70023b15d06b8322bc6267a1
Alsaad Ahamed <alsaadahamed4@gmail.com>
2020-07-17 11:08:57 +0530

    useEffect-cleanup
    - clean up is basically done by return statement in useEffect .
    - it's very important when you are doing a ajax or api call beacuse more than one request are done. So clean up is important

--- src/Counter.js
+++ src/Counter.js
@@ -1,18 +1,49 @@
-import React, { Component } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 
-class Counter extends Component {
-  render() {
-    return (
-      <div className="Counter">
-        <p className="count">0</p>
-        <section className="controls">
-          <button>Increment</button>
-          <button>Decrement</button>
-          <button>Reset</button>
-        </section>
-      </div>
-    );
-  }
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  const countRef = useRef();
+  let message = '';
+  if (countRef.current < count) message = 'higher';
+  if (countRef.current > count) message = 'lower';
+
+  countRef.current = count;
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
+  const reset = () => {
+    setCount(0);
+  };
+
+  useEffect(() => {
+    const id = setInterval(() => {
+      console.log(`count is ${count}`);
+    }, 3000);
+    return () => {
+      clearInterval(id);
+    }
+  }, [count])
+
+  return (
+    <div className="Counter">
+      {
+        count === 0 ? null : <p>{message}</p>
+      }
+      <p className="count">{count}</p>
+      <section className="controls">
+        <button onClick={increment}>Increment</button>
+        <button onClick={decrement}>Decrement</button>
+        <button onClick={reset}>Reset</button>
+      </section>
+    </div>
+  );
 }
 
 export default Counter;
